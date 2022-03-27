fba3297543c999889b921927669f2beaad0649be
Jerome Doran <disneyfan9184@gmail.com>
2022-03-27 12:59:20 -0400

    initial project setup, added image and video files

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Disney+</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Disney+</h1>
+    </div>
+  )
+}
